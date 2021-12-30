c57ca4b2946c23a452317b189430992685d048ec
vennela2003 <94883183+vennela2003@users.noreply.github.com>
2021-12-30 08:43:29 +0000

    executed

--- p4original.c
+++ p4original.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
+int input()
+{
+  int s;
+  printf("enter the value\n");
+  scanf("%d",&s);
+  return s;
+}
+int cmp(int a, int b, int c)
+{
+ if (a>b&&a>c)
+{
+  return a;
+}
+else if (b>c)
+{
+  return b;
+}
+else 
+{
+  return c;
+}
+}
+void output(int a, int b,int c,int s)
+{
+  printf("among %d,%d,%d\n %d is greatest",a,b,c,s);
+}
 int main()
 {
-    int a,b,c;
-    printf("enter 3 numbers\n");
-    scanf("%d%d%d",&a,&b,&c);
-    if((a>b)&&(a>c))
-    {
-    printf("A is big\n");
-    }
-    else if ((b>a)&&(b>c))
-    {
-    printf("B is big\n");
-    }
-    else
-    {
-    printf("C is big\n");
-    }
-    return 0;
+  int n1,n2,n3,s=0;
+  n1=input();
+  n2=input();
+  n3=input();
+  s=cmp(n1,n2,n3);
+  output(n1,n2,n3,s);
+  return 0;
 }
+
+
+
+
+
