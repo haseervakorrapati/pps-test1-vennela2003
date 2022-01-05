6afc253f3e72e0213c28a16572e1bf140b3086b4
vennela2003 <94883183+vennela2003@users.noreply.github.com>
2022-01-05 09:31:45 +0000

    executed

--- p6original.c
+++ p6original.c
@@ -3,61 +3,37 @@
 int
 input_n() 
 {
-  
-int n;
-  
-printf ("Enter a number:\n");
-  
-scanf ("%d", &n);
-  
-return n;
-
+  int n;
+  printf ("Enter a number:\n");
+  scanf ("%d", &n);
+  return n;
 }
-
-
 int
 sum_n (int n) 
 {
-  
-int sum = 0, i;
-  
-for (i = 0; i <= n; i++)
+  int sum = 0, i;
+  for (i = 0; i <= n; i++)
 {
-    
-sum = sum + i;
+    sum = sum + i;
 } 
 return sum;
-
 }
-
-
 void
 output (int n, int sum) 
 {
-  
-printf ("the sum of:\n");
-int i;
-for (i = 1; i < n; i++)
-printf ("%d +", i);
-printf ("%d", n );
-printf ("numbers is %d",sum);
- 
-
-}
-
-
+  printf ("the sum of:\n");
+ int i;
+ for (i = 1; i < n; i++)
+ printf ("%d +", i);
+ printf ("%d", n );
+ printf ("numbers is %d",sum);
+ }
 int
 main () 
 {
-  
-int n, i, sum;
-  
-n = input_n ();
-  
-sum = sum_n (n);
-  
-output (n, sum);
-  
-return 0;
-
-}
+  int n, i, sum;
+  n = input_n ();
+  sum = sum_n (n);
+  output (n, sum);
+  return 0;
+  }
